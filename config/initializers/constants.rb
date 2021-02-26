9118378d7272427b594a7a6573af3e1c7989b334
Alan McNeely <alanmcneely@gmail.com>
2021-02-26 10:06:10 -0500

    add rubocop for app and test dirs with two initial rules

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,2 @@
-ACTIVE_TAG_WEBHOOK_REQUEST_URL='https://webhook.site/a446a024-ccbc-404f-86ca-95dbdc74fa75'
-LATEST_WEBHOOK_REQUEST_URL='https://webhook.site/token/a446a024-ccbc-404f-86ca-95dbdc74fa75/request/latest'
+ACTIVE_TAG_WEBHOOK_REQUEST_URL='https://webhook.site/e0638682-c558-496f-89ed-95c757201f2f'
+LATEST_WEBHOOK_REQUEST_URL='https://webhook.site/token/e0638682-c558-496f-89ed-95c757201f2f/request/latest'
--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class TicketsControllerTest < ActionDispatch::IntegrationTest
   test "should return 422 when missing parameters" do
@@ -7,14 +7,14 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create ticket on valid parameters with no tags" do
-    assert_difference 'Ticket.count', 1 do
+    assert_difference "Ticket.count", 1 do
       post tickets_path, params: { user_id: 11, title: "Suggestion" }, as: :json
       assert_response :success
     end
   end
 
   test "should increment tag counts when valid" do
-    assert_difference 'Ticket.count', 1 do
+    assert_difference "Ticket.count", 1 do
       post tickets_path, params: { user_id: 44, title: "Two Things",
            tags: ["billing_issue","kudos"] }, as: :json
       assert_response :success
@@ -24,7 +24,7 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should return 422 if 5 or more tags" do
-    assert_no_difference 'Ticket.count' do
+    assert_no_difference "Ticket.count" do
       post tickets_path, params: { user_id: 99, title: "So Many Things",
            tags: ["billing_issue","kudos","this","that","the_other"] }, as: :json
       assert_response(422)
@@ -33,7 +33,7 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should return 422 if incorrect tag format" do
-    assert_no_difference 'Ticket.count' do
+    assert_no_difference "Ticket.count" do
       post tickets_path, params: { user_id: 50, title: "Hey There",
            tags: "should_be_array" }, as: :json
       assert_response(422)
@@ -41,7 +41,7 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should return 422 if not strings for tags" do
-    assert_no_difference 'Ticket.count' do
+    assert_no_difference "Ticket.count" do
       post tickets_path, params: { user_id: 40, title: "I Like Numbers",
            tags: [41,42,43] }, as: :json
       assert_response(422)
@@ -49,7 +49,7 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should be case-insensitive on tag names" do
-    assert_no_difference 'Tag.count' do
+    assert_no_difference "Tag.count" do
       post tickets_path, params: { user_id: 30, title: "SO MAD",
            tags: ["BILLING_ISSUE"] }, as: :json
       assert_equal Tag.find_by(name: "billing_issue").ticket_count, 4
